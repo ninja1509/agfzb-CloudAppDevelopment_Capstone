f85a59b1b7eb8ff1daac076710ce2dde1a363434
ninja1509 <63071772+ninja1509@users.noreply.github.com>
2021-10-01 12:11:38 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,28 +9,11 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
     path(route='', view=views.get_dealerships, name='index'),
-    
-    # path for about view
+    path('dealer/<int:id>/', views.get_dealer_details, name='dealer_details'),
+    path('dealer/<int:id>/review', views.add_review, name='add_review'),
     path(route='about/', view=views.about, name='about'),
-
-    # path for contact us view
-    path(route='contact/', view=views.contact, name='contact'),    # path for about view
-
-    # path for registration
-    path(route='registration/', view=views.registration_request, name='registration'),
-
-    # path for login
-    path(route='login/', view=views.login_request, name='login'),
-
-    # path for logout
-    path(route='logout/', view=views.logout_request, name='logout'),
-
-    
-
-
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
+    path(route='contact/', view=views.contact, name='contact'),
+    path('registration/', views.registration_request, name='registration'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
