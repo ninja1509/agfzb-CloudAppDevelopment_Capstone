997bdc1d60fc7cfb9160d3143149fb3a9098821b
ninja1509 <63071772+ninja1509@users.noreply.github.com>
2021-10-01 12:10:14 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,14 +1,18 @@
 from django.db import models
+from django.core import serializers
 from django.utils.timezone import now
-
+import uuid
+import json
 
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
 # - Description
-# - Any other fields you would like to include in car make model
+# - Any othere fields you would like to include in car make model
 # - __str__ method to print a car make object
+
+
 class CarMake(models.Model):
     name = models.CharField(null=False, max_length=100, default='Make')
     description = models.CharField(max_length=500)
@@ -19,14 +23,16 @@ class CarMake(models.Model):
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
-# - Dealer id, used to refer a dealer created in cloudant database
 # - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
 # - Year (DateField)
-# - Any other fields you would like to include in car model
+# - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+
+
 class CarModel(models.Model):
+    id = models.IntegerField(default=1,primary_key=True)
     name = models.CharField(null=False, max_length=100, default='Car')
-    dealer_id = models.IntegerField(default=1)
+   
     SEDAN = 'Sedan'
     SUV = 'SUV'
     WAGON = 'Wagon'
@@ -50,7 +56,67 @@ class CarModel(models.Model):
     def __str__(self):
         return "Name: " + self.name
 
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+
+class CarDealer:
+
+    def __init__(self, address, city, id, lat, long, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+       
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 
-# <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview:
+
+    def __init__(self, dealership, name, purchase, review):
+        # Required attributes
+        self.dealership = dealership
+        self.name = name
+        self.purchase = purchase
+        self.review = review
+        # Optional attributes
+        self.purchase_date = ""
+        self.purchase_make = ""
+        self.purchase_model = ""
+        self.purchase_year = ""
+        self.sentiment = ""
+        self.id = ""
+
+    def __str__(self):
+        return "Review: " + self.review
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                            sort_keys=True, indent=4)
+
+
+class ReviewPost:
+
+    def __init__(self, dealership, name, purchase, review):
+        self.dealership = dealership
+        self.name = name
+        self.purchase = purchase
+        self.review = review
+        self.purchase_date = ""
+        self.car_make = ""
+        self.car_model = ""
+        self.car_year = ""
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                            sort_keys=True, indent=4)
